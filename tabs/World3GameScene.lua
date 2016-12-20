02260761e0fbcd1ad7e7d2f17af93db19a723dae
Luke-Jeon at codea-scm <Luke-Jeon@codea-scm.aws.mapote.com>
2016-12-20 15:57:33 +0000

    commit

---
+++ tabs/World3GameScene.lua
@@ -0,0 +1,14 @@
+World3GameScene = class()
+
+function World3GameScene:init(x)
+    -- you can accept and set parameters here
+    self.x = x
+end
+
+function World3GameScene:draw()
+    -- Codea does not automatically call this method
+end
+
+function World3GameScene:touched(touch)
+    -- Codea does not automatically call this method
+end
