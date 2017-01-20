e6a15f0642388802978d2f6df6db542bbc358778
Patrick Coxall <mr.coxall@mths.ca>
2017-01-20 10:48:24 -0500

    Update settingsScene.lua

--- tabs/settingsScene.lua
+++ tabs/settingsScene.lua
@@ -1,41 +1,89 @@
--- Lesson 5
+-- ICS2O-2016-Group1
+-- settingsScene
 
--- By: Luke Jeon
--- Created November 2016
--- Created for ICS2O
--- This program displays a button and tells the user when it is selected
-
--- Local variables
-local backButton
+-- Created by: peter zhu
+-- Created on: Nov-2016
+-- Created for: ICS2O
+-- This is the project for Group #1-2016
+-- This is the third scene to show up, the game menu.
 
 SettingsScene = class()
 
+-- local variables to this scene
+--local gameCenterButton
+local gameCenterButton
+local helpButton
+local aboutUsButton
+local resetButton
+--local homeButton
+-- Use this function to perform your initial setup for this scene
 function SettingsScene:init()
-     -- set up display options
+    -- set up display options
     noFill()
     noSmooth()
     noStroke()
-    pushStyle() 
-    
-    backButton = Button("Dropbox:Blue Level Menu Button", vec2(WIDTH/13.5, HEIGHT/1.1))
-end 
+    pushStyle()
 
-function SettingsScene:touched(touch)
-    backButton:touched(touch)
-    if(backButton.selected == true) then
-        Scene.Change("gameMenu")
-        end
+   -- scene setup code here
+   -- sprite("Dropbox:Blue Back Rectangle Button")
+   -- gameCenterButton = Button("", vec2(WIDTH/2, HEIGHT/2))
+    sprite("Documents:Gold")
+    gameCenterButton =  Button("Documents:Gold", vec2(WIDTH/2, HEIGHT/1.5))
+    helpButton = Button("Documents:Gold", vec2(WIDTH/2, HEIGHT/2))
+    aboutUsButton = Button("Documents:Gold", vec2(WIDTH/2, HEIGHT/3))
+    resetButton = Button("Documents:Gold", vec2(WIDTH/2, HEIGHT/6))
+   -- homeButton = Button("Dropbox:Blue Back Rectangle Button",
+vec2(WIDTH/10, HEIGHT/1.21))
 end
 
 function SettingsScene:draw()
     -- Codea does not automatically call this method
-    sprite("Dropbox:Opening Background", WIDTH/2, HEIGHT/2, WIDTH, HEIGHT)
-    --sprite("Project:Blue Back Circle Button")
-    
-    backButton:draw()
-    fill(31, 27, 27, 255)
-    fontSize(60)
+
+    background(255, 255, 255, 255)
+    sprite("Cargo Bot:Game Lower BG",WIDTH/2, HEIGHT/2, WIDTH, HEIGHT)
+   -- gameCenterButton:draw()
+    gameCenterButton:draw()
+    helpButton:draw()
+    aboutUsButton:draw()
+    resetButton:draw()
+  --  homeButton:draw()
+    fill(0, 0, 0, 255)
+    fontSize(30)
     font("AmericanTypewriter")
-    text("Settings", WIDTH/2, HEIGHT/1.1)
-    
+    text("Game Center", WIDTH/2, HEIGHT/1.5)
+    text("Help", WIDTH/2, HEIGHT/2)
+    text("About us",WIDTH/2, HEIGHT/3)
+    text("Reset", WIDTH/2, HEIGHT/6)
+    -- Do your drawing here
+
+
 end
+
+function SettingsScene:touched(touch)
+
+        gameCenterButton:touched(touch)
+        helpButton:touched(touch)
+        aboutUsButton:touched(touch)
+        resetButton:touched(touch)
+     --homeButton:touched(touch)
+
+        if(resetButton.selected == true) then
+        Scene.Change("reset")
+
+           elseif (helpButton.selected == true) then
+          Scene.Change("help1")
+
+                 elseif(aboutUsButton.selected == true) then
+                       Scene.Change("credits")
+        end
+
+                    if (gameCenterButton.selected == true) then
+                       -- always check to ensure Game Center is logged in
+                       -- before doing a command, or the player will
+                       -- get anoying warnings!
+                            if (gamecenter.enabled() == true) then
+                             gamecenter.showLeaderboards()
+                            end
+                     end
+
+            end
