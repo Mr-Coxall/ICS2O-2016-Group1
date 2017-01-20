33febae7a30d9b68c953b95c0f3a8671e3280f04
Patrick Coxall <mr.coxall@mths.ca>
2017-01-20 10:54:32 -0500

    Update Help1Scene.lua

--- tabs/Help1Scene.lua
+++ tabs/Help1Scene.lua
@@ -1,14 +1,36 @@
-Help1Scene = class()
+HelpScene1 = class()
+-- variables local to entire file
+local moveBackButton
+local moveForwardButton
+local backButton
 
-function Help1Scene:init(x)
-    -- you can accept and set parameters here
-    self.x = x
+function HelpScene1:init()
+
+--sprite("Dropbox:Blue Backward Button")
+
+   moveBackButton = Button("Dropbox:Blue Backward Button", vec2(WIDTH/5.8, HEIGHT/2))
+   moveForwardButton = Button("Dropbox:Blue Forward Button", vec2(WIDTH/1.2, HEIGHT/2))
+   backButton = Button("Dropbox:Blue Move Scene Back Button", vec2(WIDTH/20, HEIGHT/1.07))
 end
 
-function Help1Scene:draw()
+function HelpScene1:draw()
     -- Codea does not automatically call this method
+    background(255, 255, 255, 255)
+    sprite("Project:help", WIDTH/2, HEIGHT/2, WIDTH/2, HEIGHT/2)
+     moveBackButton:draw()
+     moveForwardButton:draw()
+     backButton:draw()
 end
 
-function Help1Scene:touched(touch)
-    -- Codea does not automatically call this method
+function HelpScene1:touched(touch)
+
+    moveBackButton:touched(touch)
+    moveForwardButton:touched(touch)
+    backButton:touched(touch)
+
+    if(backButton.selected == true) then
+        Scene.Change("setting")
+    elseif(moveForwardButton.selected == true) then
+        Scene.Change("help2")
+    end
 end
